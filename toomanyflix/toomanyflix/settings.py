6fbdacd8b4ae0beab48b996bfd1a38dec10cded7
vinny-santaiti <vsantaiti@gmail.com>
2017-06-23 10:03:45 -0400

    setup travis

--- toomanyflix/toomanyflix/settings.py
+++ toomanyflix/toomanyflix/settings.py
@@ -97,12 +97,24 @@ WSGI_APPLICATION = 'toomanyflix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
 }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
